489ddfe0b47d70ae577a74531908e6a83a3d5847
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-10-14 14:17:59 +0200

    Add YelpAPI

---
+++ app/src/androidTest/java/com/example/myrestaurant/MainActivityInstrumentationTest.java
@@ -0,0 +1,43 @@
+package com.example.myrestaurant;
+
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String location = "Nairobi";
+        onView(withId(R.id.locationEditText)).perform(typeText(location));
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Here are all the restaurants near: " + location)));
+    }
+
+
+}
--- app/src/main/java/com/example/myrestaurant/MainActivity.java
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -3,35 +3,66 @@ package com.example.myrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG =MainActivity.class.getSimpleName();
-private Button mFindRestaurantsButton;
-private EditText mLocationEditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//    public static final String TAG =MainActivity.class.getSimpleName();
+//private Button mFindRestaurantsButton;
+//private EditText mLocationEditText;
+//    private TextView mAppNameTextView;
+//GridView gridView;
+//    String[] letters = new String[] {
+//            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+@BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText=(EditText)findViewById(R.id.locationEditText);
-        mFindRestaurantsButton =(Button)findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
+//        mLocationEditText=(EditText)findViewById(R.id.locationEditText);
+//        mFindRestaurantsButton =(Button)findViewById(R.id.findRestaurantsButton);
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+
+        ButterKnife.bind(this);
+//        Typeface caviarFont = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+//        mAppNameTextView.setTypeface(caviarFont);
+        mFindRestaurantsButton.setOnClickListener(this);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "font/delighter.otf"); //change
+//        gridView = (GridView) findViewById(R.id.baseGridView);
+//        gridView = (GridView) findViewById(R.id.baseGridView);
+//        gridView.setAdapter(new AlphabetAdapter(this, letters));
+    }
+//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (v==mFindRestaurantsButton){
+                Intent intent=new Intent(MainActivity.this,RestorActivity.class);
                 String location=mLocationEditText.getText().toString();
-                Log.d(TAG,location);
+//
                 Toast.makeText(MainActivity.this,location, Toast.LENGTH_LONG).show();
-                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                intent.putExtra("location",location);
                 startActivity(intent);
             }
 
 
-        });
-    }
-}
+        }
+        }
+
+
+
